a2e86c9fdc1b2381820f77f90e8e99f4d5df0254
Thavamohan Thananchayan <thananchayan26@gmail.com>
2024-06-28 22:21:14 +0530

    commit_02
    commit_02

--- back-end/springboot/src/main/java/com/project/springboot/SpringbootApplication.java
+++ back-end/springboot/src/main/java/com/project/springboot/SpringbootApplication.java
@@ -4,9 +4,11 @@ package com.project.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.project.springboot")
+@EnableRetry
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
